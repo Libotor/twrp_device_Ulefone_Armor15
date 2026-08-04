3006da8d32b96304a13f3f9f1d94d57da5c08cef
LibotorPro <dakonerne@gmail.com>
2026-08-05 00:57:15 +0200

    Create Android.mk

---
+++ Android.mk
@@ -0,0 +1,9 @@
+# Automatically generated Makefile for Ulefone Armor 15
+
+ifneq ($(filter Armor_15,$(TARGET_DEVICE)),)
+
+LOCAL_PATH := device/ulefone/Armor_15
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
